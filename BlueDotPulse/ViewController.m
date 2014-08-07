f596e76242531ee59cf655f3425a099ffd18e226
Ben Chatelain <benchatelain@gmail.com>
2014-08-07 16:36:34 -0600

    Add example pulsing blue dot animation

--- BlueDotPulse/ViewController.m
+++ BlueDotPulse/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,25 @@
 
 @implementation ViewController
             
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // http://stackoverflow.com/questions/14104727/ios-animated-pulsing-circle-like-blue-ball-on-maps#answer-14104958
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(200, 200, 100, 100)];
+    view.backgroundColor = [UIColor blueColor];
+    view.layer.cornerRadius = 50;
+
+    [self.view addSubview:view];
+
+    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation.duration = 0.2;
+    scaleAnimation.repeatCount = HUGE_VAL;
+    scaleAnimation.autoreverses = YES;
+    scaleAnimation.fromValue = [NSNumber numberWithFloat:1.2];
+    scaleAnimation.toValue = [NSNumber numberWithFloat:0.8];
+
+    [view.layer addAnimation:scaleAnimation forKey:@"scale"];
 }
 
 @end
