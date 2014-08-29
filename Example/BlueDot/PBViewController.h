d14b190d06b141609d0901f1ce90812a3728bb00
Ben Chatelain <benchatelain@gmail.com>
2014-08-29 10:06:29 -0600

    Add custom view controller to example

---
+++ Example/BlueDot/PBViewController.h
@@ -0,0 +1,13 @@
+//
+//  PBViewController.h
+//  BlueDot
+//
+//  Created by Ben Chatelain on 8/29/14.
+//  Copyright (c) 2014 Ben Chatelain. All rights reserved.
+//
+
+@import UIKit.UIViewController;
+
+@interface PBViewController : UIViewController
+
+@end
---
+++ Example/BlueDot/PBViewController.m
@@ -0,0 +1,37 @@
+//
+//  PBViewController.m
+//  BlueDot
+//
+//  Created by Ben Chatelain on 8/29/14.
+//  Copyright (c) 2014 Ben Chatelain. All rights reserved.
+//
+
+#import "PBViewController.h"
+
+@implementation PBViewController
+
+#pragma mark - NSObject (UINibLoadingAdditions)
+
+- (void)awakeFromNib
+{
+
+}
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+@end
